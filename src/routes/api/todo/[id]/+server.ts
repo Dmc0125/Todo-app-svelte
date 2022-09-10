f6da124f6ea20cd4b9c2beeba3725af485156fd4
dmc0125 <domikmic@gmail.com>
2022-09-10 21:53:10 +0200

    PUT todo as complete

--- src/routes/api/todo/[id]/+server.ts
+++ src/routes/api/todo/[id]/+server.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
 
 import type { RequestHandler } from './$types'
 import { requiredError, typeError } from '$lib/utils/zod'
@@ -37,5 +38,61 @@ export const GET: RequestHandler = async ({ params, url }) => {
     return jsonResponse(todo)
   } catch (error) {
     return jsonResponse('Server error')
+  }
+}
+
+const setDoneSchema = z.object({
+  done: z.boolean(),
+  groupId: z.number({
+    required_error: requiredError('groupId'),
+    invalid_type_error: typeError('groupId', 'number'),
+  }).min(1),
+}).merge(getSchema.pick({ todoId: true }))
+
+export const PUT: RequestHandler = async ({ request, params }) => {
+  const body = await request.json()
+  const result = setDoneSchema.safeParse({
+    ...body,
+    todoId: params.id,
+  })
+  if (!result.success) {
+    return zodErrorResponse(result.error)
+  }
+
+  const { groupId, todoId, done } = result.data
+
+  try {
+    const [todo] = await prismaClient.$transaction([
+      prismaClient.todo.update({
+        where: {
+          id: todoId,
+        },
+        data: {
+          done,
+        },
+      }),
+      prismaClient.todoGroup.update({
+        where: {
+          id: groupId,
+        },
+        data: {
+          todosCompleted: { increment: 1 },
+        },
+      }),
+    ])
+    return jsonResponse(todo)
+  } catch (error) {
+    if (
+      error instanceof PrismaClientKnownRequestError
+      && error.meta?.cause === 'Record to update not found.'
+    ) {
+      return jsonResponse('Todo or todoGroup not found.', {
+        init: {
+          status: 404,
+        },
+        success: false,
+      })
+    }
+    return jsonResponse('Server error.')
   }
 }
